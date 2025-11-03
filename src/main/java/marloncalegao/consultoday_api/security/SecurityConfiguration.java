d69201bc751458682dc8bf1d2ea394868ff852ef
marloncalegao <marlon.calegao86@hotmail.com>
2025-11-03 15:26:40 -0300

    Adicionado bean para CORS no SecurityConfiguration

--- src/main/java/marloncalegao/consultoday_api/security/SecurityConfiguration.java
+++ src/main/java/marloncalegao/consultoday_api/security/SecurityConfiguration.java
@@ -6,12 +6,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     private final SecurityFilter securityFilter;
@@ -42,6 +46,20 @@ public class SecurityConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Aplica para todas as rotas que começam com /api
+                        .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://seusite.com") // Endereços das suas aplicações web e mobile (em produção)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Permite todos os headers
+                        .allowCredentials(true); // Se você usar cookies ou autorização com credenciais
+            }
+        };
     }
 
 }
